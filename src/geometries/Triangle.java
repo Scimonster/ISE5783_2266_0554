66407ce02a7f9a251396ced5886658cabaf13664
AriBlumstein <ayb1998@aol.com>
2023-03-02 16:17:00 +0200

    hello

---
+++ src/geometries/Triangle.java
@@ -0,0 +1,17 @@
+package geometries;
+import primitives.Point;
+
+import java.util.List;
+
+public class Triangle extends Polygon {
+    public Triangle(Point p1, Point p2, Point p3)
+    {
+        super(p1,p2,p3);
+    }
+
+    public List<Point> getPoints()
+    {
+        return this.vertices;
+    }
+
+}
