8a5966996b4f5a9a8ec5c89df9115878eb05a5b8
Scimonster <tehalmightyscimonster@gmail.com>
2023-03-20 14:04:56 +0200

    Add missing plane tests

--- src/unittests/geometries/PlaneTest.java
+++ src/unittests/geometries/PlaneTest.java
@@ -16,6 +16,7 @@ class PlaneTest {
     @Test
     void getNormal() {
         // ============ Equivalence Partitions Tests =======
+        // xy plane
         Point p1 = new Point (1,0,0);
         Point p2 = new Point(0,1, 0);
         Point p3 = new Point (1,1,0);
@@ -25,5 +26,20 @@ class PlaneTest {
 
         assertThat(cur.getNormal(p1), AnyOf.anyOf(IsEqual.equalTo(v1),IsEqual.equalTo(v2)));
 
+    }
+
+    @Test
+    void testConstructor() {
+        Point p1 = new Point (1,0,0);
+        Point p2 = new Point(0,1, 0);
+        // =============== Boundary Values Tests ==================
+        // first and second points equal
+        assertThrows(IllegalArgumentException.class, () -> new Plane(p1, p1, p2), "should not create plane with points being equal");
+
+        // all points in a line
+        Point p3 = new Point (1,0,0);
+        Point p4 = new Point(2,0, 0);
+        Point p5 = new Point(3,0, 0);
+        assertThrows(IllegalArgumentException.class, () -> new Plane(p3, p4, p5), "should not create plane with 3 colinear points");
     }
 }
