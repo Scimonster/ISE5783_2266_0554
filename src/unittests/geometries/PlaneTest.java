a36d1c8c1ab343995f4c8ccf9a9d729a27f84084
Scimonster <tehalmightyscimonster@gmail.com>
2023-03-28 17:57:05 +0300

    findIntersections plane tests

--- src/unittests/geometries/PlaneTest.java
+++ src/unittests/geometries/PlaneTest.java
@@ -8,6 +8,7 @@ import geometries.Plane;
 
 import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
 class PlaneTest {
     /**
@@ -49,6 +50,48 @@ class PlaneTest {
     @Test
     void testFindIntersections()
     {
-        fail("not yet implemented");
+        // plane z=1, with reference point (0,0,1)
+        Plane plane = new Plane(new Point(0, 0, 1), new Vector(0,0,1));
+        List<Point> result;
+        // ============ Equivalence Partitions Tests =======
+        // Ray is neither orthogonal nor parallel to the plane
+        // TC01: Ray intersects the plane (1 point)
+        result = plane.findIntersections(new Ray(new Point(1,1,2), new Vector(-1, -1, -1)));
+        assertEquals(1, result.size(), "Wrong number of points");
+        assertEquals(new Point(0,0,1), result.get(0), "Unexpected point");
+        // TC02: Ray does not intersect the plane (0 points)
+        result = plane.findIntersections(new Ray(new Point(1,1,2), new Vector(1, 1, 1)));
+        assertNull(result, "Should return 0 points");
+
+        // =============== Boundary Values Tests ==================
+        // Group: ray is parallel to the plane (no points)
+        // TC03: Ray is included in the plane
+        result = plane.findIntersections(new Ray(new Point(0,0,1), new Vector(1, 1, 0)));
+        assertNull(result, "Should return 0 points");
+
+        // TC04: Ray is not included in the plane
+        result = plane.findIntersections(new Ray(new Point(0,0,2), new Vector(1, 1, 0)));
+        assertNull(result, "Should return 0 points");
+
+        // Group: ray is orthogonal to the plane
+        // TC05: P0 is before the plane (1 point)
+        result = plane.findIntersections(new Ray(new Point(1,0,0), new Vector(0, 0, 1)));
+        assertEquals(1, result.size(), "Wrong number of points");
+        assertEquals(new Point(1,0,1), result.get(0), "Unexpected point");
+        // TC06: P0 is on the plane (0 points)
+        result = plane.findIntersections(new Ray(new Point(1,0,1), new Vector(0, 0, 1)));
+        assertNull(result, "Should return 0 points");
+        // TC07: P0 is after the plane
+        result = plane.findIntersections(new Ray(new Point(1,0,2), new Vector(0, 0, 1)));
+        assertNull(result, "Should return 0 points");
+
+        // Group: special cases
+        // TC08: Ray is neither orthogonal nor parallel to and begins at the plane
+        result = plane.findIntersections(new Ray(new Point(1,0,1), new Vector(1, 1, 1)));
+        assertNull(result, "Should return 0 points");
+
+        // TC09: Ray begins in the plane's reference point, not parallel or orthogonal
+        result = plane.findIntersections(new Ray(new Point(0,0,1), new Vector(1, 1, 1)));
+        assertNull(result, "Should return 0 points");
     }
 }
