174f334437f81335400acc008fe9a85d20d09079
Scimonster <tehalmightyscimonster@gmail.com>
2023-04-14 15:04:28 +0300

    Fix triangle test math

--- src/unittests/geometries/TriangleTest.java
+++ src/unittests/geometries/TriangleTest.java
@@ -42,8 +42,11 @@ class TriangleTest {
         Triangle T1 = new Triangle(new Point(1,0,1), new Point(1,1,1), new Point(0,1,1));
         // ============ Equivalence Partitions Tests =======
 
+        //TC00: Ray does not intersect triangle's plane
+        assertNull(T1.findIntersections(new Ray(new Point(0,0,0), new Vector(1, 0, 0))));
+
         //TC01: Ray does not intersect triangle (ray passes by a side of the triangle in plane that triangle is found) (0 points)
-        assertNull(T1.findIntersections(new Ray(new Point(0,2,0), v1)), "Should return 0 points");
+        assertNull(T1.findIntersections(new Ray(new Point(0.5,2,0), v1)), "Should return 0 points");
 
 
         //TC02: Ray does not intersect triangle (Intersection is found "by corner" of triangle, between 2 side "continuations) (0 points)
@@ -51,19 +54,19 @@ class TriangleTest {
 
         //TC03: Ray intersects triangle (within the triangle) (1 point)
         Point p1= new Point(0.5,0.5,1);
-        List<Point> result=T1.findIntersections(new Ray(new Point(0.5, 0.5, 0), v1));
+        List<Point> result=T1.findIntersections(new Ray(new Point(0.9, 0.9, 0), v1));
         assertEquals(1,result.size(), "Returned wrong number of points");
         assertEquals(result, List.of(p1), "Returned wrong point");
 
         // =============== Boundary Values Tests ==================
         //TC04: Intersection is on a side of triangle (0 points)
-        assertNull(T1.findIntersections(new Ray(new Point(0.5, 1, 0), v1)), "Should return 0 points");
+        assertNull(T1.findIntersections(new Ray(new Point(0, 1, 0), v1)), "Should return 0 points");
 
         //TC05: Intersection is on a corner of triangle (0 points)
         assertNull(T1.findIntersections((new Ray(new Point(1,1,0), v1 ))), "Should return 0 points" );
 
         //TC06: Intersection is on "continuation of a side" of the triangle (0 points)
-        assertNull(T1.findIntersections(new Ray(new Point(2,1,0), v1)), "Should return 0 points");
+        assertNull(T1.findIntersections(new Ray(new Point(2,0,0), v1)), "Should return 0 points");
 
 
 
