01d3692a83b44700af20c74145a4b516fc7f868e
Scimonster <tehalmightyscimonster@gmail.com>
2023-04-17 22:58:44 +0300

    Don't return immutable copy

--- src/geometries/Geometries.java
+++ src/geometries/Geometries.java
@@ -2,7 +2,6 @@ package geometries;
 
 import primitives.*;
 
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -33,8 +32,8 @@ public class Geometries implements Intersectable{
     @Override
     public List<Point> findIntersections(Ray ray)
     {
-         // 2 lists, one to hold the current shapes intersections, and one to hold all the intersections
-         List<Point> preres=null, cur=null;
+         // 2 lists, one to hold the current shape's intersection points, and one to hold all the intersections
+         List<Point> res=null, cur=null;
 
          //iterate through whole this.shapes, invoke findIntersections for each shape
          for (Intersectable shape: this.shapes)
@@ -42,26 +41,18 @@ public class Geometries implements Intersectable{
              cur=shape.findIntersections(ray);
              if(cur!=null)
              {
-                 if (preres==null)
+                 if (res==null)
                  {
-                     preres=new LinkedList<>(cur);
+                     res=new LinkedList<>(cur);
                  }
                  else
                  {
-                     preres.addAll(cur);
+                     res.addAll(cur);
                  }
              }
          }
 
-         //if we found no intersections, return null
-         if (preres==null)
-         {
-             return null;
-         }
-
-
-         //return an immutable form of all the intersections
-         return Collections.unmodifiableList(preres);
+         return res;
 
     }
 }
