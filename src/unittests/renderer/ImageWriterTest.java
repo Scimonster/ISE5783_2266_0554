6d040fedbd1ce8ba4ab21edc257409f5ca669f08
Scimonster <tehalmightyscimonster@gmail.com>
2023-05-01 22:15:03 +0300

    Stage5 phase1
    Color, ImageWriter, and test

---
+++ src/unittests/renderer/ImageWriterTest.java
@@ -0,0 +1,39 @@
+package unittests.renderer;
+
+import org.junit.Test;
+import primitives.Color;
+import renderer.ImageWriter;
+
+/**
+ * Test ImageWriter
+ */
+public class ImageWriterTest {
+    /**
+     * Build a 10x16 grid at resolution 500x800
+     */
+    static final int NX = 800;
+    static final int NY = 500;
+    static final int WIDTH = 16;
+    static final int HEIGHT = 10;
+    @Test
+    public void testImageWriter()
+    {
+        Color bg = new Color(255, 0, 0);
+        Color fg = new Color(0, 0, 255);
+        ImageWriter iw = new ImageWriter("grid", NX, NY);
+
+        int xScale = NX / WIDTH;
+        int yScale = NY / HEIGHT;
+
+        for (int j = 0; j < NX; j++) {
+            for (int i = 0; i < NY; i++) {
+                if (j % xScale == 0 || i % yScale == 0) { // grid line
+                    iw.writePixel(j, i, fg);
+                } else {
+                    iw.writePixel(j, i, bg);
+                }
+            }
+        }
+        iw.writeToImage();
+    }
+}
