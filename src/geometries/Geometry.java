6c514d628a2da1874d19aaee9ed0545637785217
AriBlumstein <ayb1998@aol.com>
2023-05-09 16:42:15 +0300

    refactored Geometry to include emission color

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -1,4 +1,5 @@
 package geometries;
+import primitives.Color;
 import primitives.Point;
 import primitives.Ray;
 import primitives.Vector;
@@ -9,6 +10,9 @@ import java.util.List;
  * A generic Geometry object in our model
  */
 public abstract class Geometry implements Intersectable {
+
+    protected Color emission = Color.BLACK;
+
     /**
      * Get the normal vector to the geometry at a given point
      * @param point Point to get the normal based on
@@ -17,4 +21,26 @@ public abstract class Geometry implements Intersectable {
     abstract Vector getNormal(Point point);
 
     abstract public List<Point> findIntersections(Ray ray);
+
+    /**
+     * method that returns the emission color
+     * @return
+     */
+    public Color getEmission() {
+        return emission;
+    }
+
+    /**
+     * method to set emission color
+     * @param emit
+     * @return the object itself
+     */
+
+    public Geometry setEmission(Color emit)
+    {
+        if (emit == null)
+            throw new IllegalArgumentException("can't give a null color");
+        this.emission=emit;
+        return this;
+    }
 }
